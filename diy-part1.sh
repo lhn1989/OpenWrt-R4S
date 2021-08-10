ab2d0199cb96eae90b885462fec2ff22527967dd
lhn1989 <lhn1989@users.noreply.github.com>
2021-08-10 14:49:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,12 +13,8 @@
 # Print Commands
 set -v
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add openclash
 mkdir openclash
