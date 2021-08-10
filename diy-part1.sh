15df919f0c0056a53423bae18639a2e927b10d29
lhn1989 <lhn1989@users.noreply.github.com>
2021-08-10 12:12:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,7 +26,6 @@ git remote add origin https://github.com/vernesong/OpenClash.git
 git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
 mv luci-app-openclash ../package/
 cd ..
 rm -rf openclash
